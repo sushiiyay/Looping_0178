2cd54db13cc1035037d27b436b3c21d64fe6de31
sushiiyay <nfa.aliya7811@gmail.com>
2026-03-30 17:03:56 +0800

    //Perulangan dengan DO...WHILE

--- looping.cpp
+++ looping.cpp
@@ -16,4 +16,17 @@ int main(){
     srand(time(0));
 
     //Perulangan dengan WHILE
+    cout<<"PERULANGAN WHILE"<<endl;
+    x = 1 + rand()%10;
+
+    while(x<=5){
+        cout<<"Bilangan acak = "<<x <<endl;
+        x = rand()%10;
+    }
+
+    cout<<"Bilangan acak  while yang terakhir ="<<x <<endl;
+    cout<<endl;
+
+    //Perulangan dengan DO...WHILE
+    
 } 
