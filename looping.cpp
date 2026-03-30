2467947a7d4555c6a8a97701e93da93d05188f5e
sushiiyay <nfa.aliya7811@gmail.com>
2026-03-30 16:57:35 +0800

    //untuk mendapatkan urutan angka acak yang berbeda

--- looping.cpp
+++ looping.cpp
@@ -7,4 +7,10 @@ int main(){
 
     //Perulangan dengan FOR
     //mencetak tulisan sebanyak 5 kali
-}
+    cout<<"PERULANGAN FOR"<<endl
+    for(int i=0;i<=4;i++){
+        cout<<"Teknologi Informasi UMY"<<endl;
+    }
+    cout<<endl;
+//untuk mendapatkan urutan angka acak yang berbeda
+} 
